b48db716ded18674b87442fbe40bfb6b530b5ccb
Colin Rundel <rundel@gmail.com>
2024-10-08 13:29:31 -0400

    Make update

--- docs/slides/Lec12_notes.R
+++ docs/slides/Lec12_notes.R
@@ -14,17 +14,24 @@ movies = tibble::tibble(
     html_elements(".dynamic-text-list__streaming-links+ ul .dynamic-text-list__item-title") |>
     html_text2(),
   tomatometer = page |>
-    html_elements(".dynamic-text-list__streaming-links+ ul .b--medium") |>
+    html_elements(".dynamic-text-list__streaming-links+ ul rt-text") |>
     html_text2() |>
     str_remove("%$") |>
     as.numeric() |>
     (\(x) x/100)(),
-  status = page |>
-    html_elements(".dynamic-text-list__streaming-links+ ul .icon--tiny") |>
-    html_attr("class") |>
-    str_remove("icon ") |>
-    str_remove("icon--tiny ") |>
-    str_remove("icon__"),
+  certified = page |>
+    html_elements(".dynamic-text-list__streaming-links+ ul score-icon-critics") |>
+    html_attr("certified") |>
+    str_to_upper() |>
+    as.logical(),
+  sentiment = page |>
+    html_elements(".dynamic-text-list__streaming-links+ ul score-icon-critics") |>
+    html_attr("sentiment"),
+  status = case_when(
+     certified & sentiment == "positive" ~ "certified fresh",
+    !certified & sentiment == "positive" ~ "fresh",
+     sentiment == "negative" ~ "rotten"
+  ),
   url = page |> 
     html_elements(".dynamic-text-list__streaming-links+ ul li a.dynamic-text-list__tomatometer-group") |>
     html_attr("href") |>
@@ -43,29 +50,28 @@ scrape_movie_page = function(url) {
   
   list(
     mpaa_rating = page |>
-      html_elements(".info-item:nth-child(1) span") |>
-      html_text2() |>
-      str_remove(" \\(.*\\)"),
-    
-    runtime = page |>
-      html_elements(".info-item:nth-child(10) time") |>
+      html_elements(".unset+ rt-text") |>
       html_text2(),
     
-    tomatometer_score = page |>
-      html_elements("#scoreboard") |>
-      html_attr("tomatometerscore") |>
-      as.integer(),
+    runtime = page |>
+      html_elements("#hero-wrap rt-text:nth-child(4)") |>
+      html_text2(),
     
     audience_score = page |>
-      html_elements("#scoreboard") |>
-      html_attr("audiencescore") |>
+      html_elements("rt-button:nth-child(7) rt-text") |> 
+      html_text2() |>
+      str_remove("%$") |>
       as.integer(),
 
     n_reviews = page |>
-      html_elements('#scoreboard > a[data-qa="tomatometer-review-count"]') |>
+      html_elements('.critics-score-type+ rt-link') |>
       html_text2() |>
-      str_remove(" Reviews") |>
-      as.integer()
+      str_remove(" Reviews"),
+    
+    n_aud_ratings = page |>
+      html_elements('.audience-score-type+ rt-link') |>
+      html_text2() |>
+      str_remove(" Ratings")
   )
 }
 
